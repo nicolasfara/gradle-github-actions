29493dd696d9d68bbff87e5ebc924365b07ab80c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-11-15 08:10:42 +0100

    Some improvements

--- plugin/src/main/kotlin/gghactions/model/github/Step.kt
+++ plugin/src/main/kotlin/gghactions/model/github/Step.kt
@@ -1,22 +1,19 @@
 package gghactions.model.github
 
-import com.charleskorn.kaml.PolymorphismStyle
-import com.charleskorn.kaml.Yaml
-import com.charleskorn.kaml.YamlConfiguration
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class Step {
     abstract var name: String
-    abstract var `if`: String
-    abstract var env: Map<String, String>
+    abstract var `if`: String?
+    abstract var env: Map<String, String>?
 }
 
 @Serializable
 data class Cli(
     override var name: String,
-    override var `if`: String = "",
-    override var env: Map<String, String> = emptyMap(),
+    override var `if`: String? = null,
+    override var env: Map<String, String>? = null,
     val shell: String = "",
     val run: String,
 ) : Step()
@@ -24,8 +21,8 @@ data class Cli(
 @Serializable
 data class Gradle(
     override var name: String,
-    override var `if`: String = "",
-    override var env: Map<String, String> = emptyMap(),
+    override var `if`: String? = null,
+    override var env: Map<String, String>? = null,
     @Serializable(with = TaskToStringSerializer::class)
     val tasks: List<String>
 ) : Step()
@@ -33,16 +30,8 @@ data class Gradle(
 @Serializable
 data class Action(
     override var name: String,
-    override var `if`: String = "",
-    override var env: Map<String, String> = emptyMap(),
+    override var `if`: String? = null,
+    override var env: Map<String, String>? = null,
     val uses: String,
     val with: Map<String, String> = emptyMap(),
 ) : Step()
-
-fun main() {
-    val gradle = Gradle(name = "Hello", tasks = listOf("check"))
-    val job = Job(name = "okk", steps = listOf(gradle))
-    val config = YamlConfiguration(encodeDefaults = false, polymorphismStyle = PolymorphismStyle.Property)
-    print(Yaml(configuration = config).encodeToString(Job.serializer(), job))
-}
-
