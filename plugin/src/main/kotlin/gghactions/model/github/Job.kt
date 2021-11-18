1d2393a49c9850c43f4fafbc48c18fe34d368a4a
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-11-18 12:00:20 +0100

    Add template files

--- plugin/src/main/kotlin/gghactions/model/github/Job.kt
+++
@@ -1,6 +0,0 @@
-package gghactions.model.github
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Job(var name: String, var `runs-on`: List<String> = listOf("ubuntu-latest"), var steps: List<Step> = emptyList())
