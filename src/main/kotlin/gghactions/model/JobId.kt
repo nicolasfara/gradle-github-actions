79e5a86fc99b5889a9483684708770e9b0ce4098
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-12-20 11:24:50 +0100

    style: refactor class style

--- src/main/kotlin/gghactions/model/JobId.kt
+++ src/main/kotlin/gghactions/model/JobId.kt
@@ -1,6 +1,7 @@
 package gghactions.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import gghactions.configuration.DefaultValues
 
 /**
  * TODO.
@@ -14,10 +15,15 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class JobId(
     var needs: List<String>? = null,
     @JsonProperty("runs-on")
-    var runsOn: String,
+    var runsOn: String = "\${{ matrix.os }}",
     var env: Map<String, String>? = null,
     @JsonProperty("if")
     var cond: String? = null, // ktlint-disable
-    var strategy: Matrix = Matrix(mutableMapOf("os" to listOf("ubuntu-latest"), "java-version" to listOf(17))),
+    var strategy: Matrix = Matrix(
+        mutableMapOf(
+            "os" to listOf(DefaultValues.defaultOs),
+            "java-version" to listOf(DefaultValues.defaultJavaVersion)
+        )
+    ),
     var steps: List<Step>
 )
