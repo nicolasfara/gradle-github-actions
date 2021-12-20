228fe4680c15ee4e38e37913de3ece28a1d01841
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-12-20 11:23:03 +0100

    docs: add javadoc

--- src/main/kotlin/gghactions/GithubActionsExtension.kt
+++ src/main/kotlin/gghactions/GithubActionsExtension.kt
@@ -8,26 +8,46 @@ import org.gradle.api.Project
  * Container for the GitHub actions plugin configuration DSL.
  * [project] is the Gradle project.
  */
-open class GithubActionsExtension(private val project: Project) {
+open class GithubActionsExtension(val project: Project) {
 
     internal val workflow = Workflow.defaultConfig()
 
+    /**
+     * The workflow's name.
+     */
     var name: String
         get() = workflow.name
         set(value) { workflow.name = value }
 
+    /**
+     * The trigger policy for this workflow.
+     */
     var on: Map<String, Any>
         get() = workflow.on
         set(value) { workflow.on = value }
 
+    /**
+     * Set up the Os types used in the workflow.
+     * The default available os are: `ubuntu-latest`, `macos-latest`, `windows-latest`.
+     */
+    @Suppress("UNCHECKED_CAST")
     var os: List<String>
         get() = workflow.jobs.build.strategy.matrix["os"] as List<String>
         set(value) { workflow.jobs.build.strategy.matrix["os"] = value }
 
+    /**
+     * Set up the Java version used in the workflow.
+     * The version should be compatible with the version accepted by the action: `actions/setup-java`.
+     */
+    @Suppress("UNCHECKED_CAST")
     var java: List<Int>
         get() = workflow.jobs.build.strategy.matrix["java-version"] as List<Int>
         set(value) { workflow.jobs.build.strategy.matrix["java-version"] = value }
 
+    /**
+     * Configure the build step of the workflow.
+     * This step allows adding more steps in the building job.
+     */
     fun build(config: Steps.() -> Unit) {
         val steps = Steps()
         steps.config()
--- src/main/kotlin/gghactions/configuration/Steps.kt
+++ src/main/kotlin/gghactions/configuration/Steps.kt
@@ -4,17 +4,47 @@ import gghactions.model.Action
 import gghactions.model.Cli
 import gghactions.model.Step
 
-abstract class StepsConf {
+/**
+ * DSL class used to build a step in the workflow.
+ */
+class Steps {
     internal val userSteps = arrayListOf<Step>()
 
-    fun <T : Step> initStep(step: T, config: T.() -> Unit): T {
+    private fun <T : Step> initStep(step: T, config: T.() -> Unit): T {
         step.config()
         userSteps.add(step)
         return step
     }
-}
 
-class Steps : StepsConf() {
+    /**
+     * A command line command in the job.
+     *
+     * ```
+     * build {
+     *   cli {
+     *     name = "Some task"
+     *     run = "echo hello world"
+     *   }
+     * }
+     * ```
+     *
+     * Refer to [Cli] class for a more detailed configuration options.
+     */
     fun cli(init: Cli.() -> Unit) = initStep(Cli(name = "Changeme", run = "changeme"), init)
+
+    /**
+     * Use of an action as a step in the job.
+     *
+     * ```
+     * build {
+     *   action {
+     *     name = "Some action"
+     *     uses = "foo/bar@v1"
+     *   }
+     * }
+     * ```
+     *
+     * Refer to [Action] class for a more detailed configuration option.
+     */
     fun action(init: Action.() -> Unit) = initStep(Action(name = "Changeme", uses = "changeme"), init)
 }
